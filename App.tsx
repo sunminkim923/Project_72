d3f37375a4f3ae6d514b6a130a26ff4c0e0e7fd2
junku ji <doorly01@gmail.com>
2021-09-14 16:10:21 +0900

    Fix 소셜,일반로그인 통합 및 로그아웃 수정

--- App.tsx
+++ App.tsx
@@ -9,12 +9,23 @@ import MainBottomTabNavigationPage from './pages/navigation/MainBottomTabNavigat
 import {useState, createContext} from 'react';
 import StartPageStackNavigationPage from './pages/navigation/StartPageStackNavigation';
 import {createUploadLink} from 'apollo-upload-client';
+import {useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const GlobalContext = createContext({});
 
 function App() {
   const [accessToken, setAccessToken] = useState('');
-  const [userInfo, setUserInfo] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    if (AsyncStorage.getItem('accessToken')) {
+      setAccessToken(accessToken);
+    }
+    if (AsyncStorage.getItem('userInfo')) {
+      setUserInfo(AsyncStorage.getItem('userInfo'));
+    }
+  }, []);
 
   const uploadLink = createUploadLink({
     uri: 'http://34.68.72.16:4000/graphql',
@@ -28,7 +39,6 @@ function App() {
     link: ApolloLink.from([uploadLink as unknown as ApolloLink]),
     cache: new InMemoryCache(),
   });
-
   const value = {
     accessToken: accessToken,
     setAccessToken: setAccessToken,
