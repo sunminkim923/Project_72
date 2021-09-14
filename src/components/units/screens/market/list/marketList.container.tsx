67906967d3fd3c63fe8756cb32b4783a611e987e
Bu-Du-Dak <guri930219@gmail.com>
2021-09-14 22:36:24 +0900

    Add MarketList API

--- src/components/units/screens/market/list/marketList.container.tsx
+++ src/components/units/screens/market/list/marketList.container.tsx
@@ -1,11 +1,37 @@
 import {useQuery} from '@apollo/client';
-import React from 'react';
+import React, {useState} from 'react';
 import MarketListUI from './marketList.presenter';
 import {FETCH_USED_ITEMS} from './marketList.queries';
 
 const MarketList = (props: any) => {
-  const {data} = useQuery(FETCH_USED_ITEMS);
-
-  return <MarketListUI navigation={props.navigation} data={data} />;
+  const {data, fetchMore} = useQuery(FETCH_USED_ITEMS);
+  const [hasMore, setHasMore] = useState(true);
+  const onLoadMore = () => {
+    console.log('nnn', data?.fetchUseditems.length);
+    console.log('page', Math.ceil(data?.fetchUseditems.length / 10) + 1);
+    fetchMore({
+      variables: {
+        page: Math.ceil(data?.fetchUseditems.length / 10) + 1,
+      },
+      updateQuery: (prev, {fetchMoreResult}) => {
+        console.log('ffffffff', fetchMoreResult);
+        if (!fetchMoreResult.fetchUseditems.length) setHasMore(false);
+        return {
+          fetchUseditems: [
+            ...prev.fetchUseditems,
+            ...fetchMoreResult.fetchUseditems,
+          ],
+        };
+      },
+    });
+  };
+  return (
+    <MarketListUI
+      navigation={props.navigation}
+      data={data}
+      onLoadMore={onLoadMore}
+      hasMore={hasMore}
+    />
+  );
 };
 export default MarketList;
